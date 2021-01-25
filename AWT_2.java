1aeb672554bcf8ce5cc3d26ceb80004fcf9b2cf6
Keshav612 <500062098@stu.upes.ac.in>
2021-01-25 12:20:24 +0530

    Closing frame functionality on click.

---
+++ AWT_2.java
@@ -0,0 +1,37 @@
+//Closing frame on click of button
+//Keshav Mishra
+import java.awt.*;
+import java.awt.event.*;
+
+public class AWT_2 extends Frame  {
+    AWT_2(){
+        Frame f= new Frame("Title");
+        f.setSize(300,600);
+    Button b = new Button("CLick me");
+    setTitle("Hello! I am Frame");
+    b.setBounds(30,200,80,30);
+    add(b);
+    setSize(600,300);
+    setLayout(null);
+    setVisible(true);
+        addWindowListener(new WindowAdapter() {
+           
+            public void windowClosing(WindowEvent e)
+            {dispose();
+            }
+
+
+        });
+
+        
+        
+        
+    }
+
+    public static void main(String[] args) {
+        new AWT_2();
+        
+        
+    }
+    
+}
