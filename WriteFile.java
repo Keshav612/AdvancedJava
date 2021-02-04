7a73711cdcee60cdaefb6d78f04406b6c9d2fb8e
Keshav612 <500062098@stu.upes.ac.in>
2021-02-04 23:11:18 +0530

    Writing to a file.

---
+++ WriteFile.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class WriteFile 
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter text to be written");
+        String s = in.next();
+        try{
+        FileWriter obj = new FileWriter("file1.txt");
+        obj.write(s);
+        obj.close();
+        System.out.println("Successful");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Unsuccesful");
+        }
+    }    
+}
