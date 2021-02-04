719c57f367147f78f795940ba2f51535978e3885
Keshav612 <500062098@stu.upes.ac.in>
2021-02-04 23:29:15 +0530

    Changes to write whole text.

--- WriteFile.java
+++ WriteFile.java
@@ -5,7 +5,7 @@ public class WriteFile
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter text to be written");
-        String s = in.next();
+        String s = in.nextLine();
         try{
         FileWriter obj = new FileWriter("file1.txt");
         obj.write(s);
